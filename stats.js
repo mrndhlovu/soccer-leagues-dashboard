4261ca56dc60a176aef14a10728f6666c3265a36
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2019-01-02 17:26:48 +0000

    new sport app

--- stats.js
+++ stats.js
@@ -1,91 +1,85 @@
-var getData = "https://api.got.show/api/characters/";
-
-function getDataFromAPI(callBack) {
-    var getApiData = new XMLHttpRequest();
-
-    getApiData.open("GET", getData);
-    getApiData.send();
-
-    getApiData.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            callBack(JSON.parse(this.responseText));
-        }
-    };
-}
 //Log to console function
 function print(data) {
     console.log(data);
 }
 
-//Api object variables
-var characters = [];
-var house = [];
-var gender = [];
-var book = [];
-var clear = " ";
-
-
-// Get data and push to empty arrays
-function useApiData(data) {
+var apiRequestURL = "https://api.football-data.org/v2/competitions/PL/matches";
+var apiAuthorization = { 'X-Auth-Token': '5d791d1818c3415d9b1a4b323c899bf4' };
 
 
 
-    // Iterate though api array and fill empty arrays
-    data.forEach(function(item, index, array) {
+$.ajax({
+    headers: apiAuthorization,
+    url: apiRequestURL,
+    dataType: 'json',
+    type: 'GET',
+}).done(function(response) {
+    // do something with the response, e.g. isolate the id of a linked resource   
+    var data = response;
+    var match = data.matches;
 
-        characters.push(item.name);
-        // document.getElementById("data").innerHTML = characters;
 
-        house.push(item.house);
-        //document.getElementById("data").innerHTML = house;
+    // empty arrays
+    var awayTeam = [];
+    var homeTeam = [];
+    var matchDay = [];
+    var awayScore = [];
+    var homeScore = [];
+    var status = [];
 
-        var sex = item.male
-        if (sex === true) {
-            sex = "Male";
-            gender.push(sex);
-        }
-        else if (sex === false) {
-            sex = "Female";
-            gender.push(sex);
+
+
+    print(match[0].status);
+    //Loop through object arrays and filter data using push to empty arrays
+    Object.keys(match).forEach(function(key) {
+
+        matchDay.push(match[key].matchday);
+
+        awayTeam.push(match[key].awayTeam.name);
+        status.push(match[key].status);
+
+
+        //Populate home and away score arrays
+        if (match[key].score) {
+            awayScore.push(match[key].score.fullTime.awayTeam);
         }
         else {
-            sex = "Not Specified";
-            gender.push(sex);
+            homeScore.push(match[key].score.fullTime.homeTeam);
         }
-        //document.getElementById("data").innerHTML = gender;
 
-        book.push(item.books);
-        //document.getElementById("data").innerHTML = book;
 
 
     });
-}
 
-// Write to document
-function showNames() {
-    //
-    document.getElementById("infoBox").innerHTML = characters;
-}
+    for (var i = 0 ; i < matchDay.length; i++) {
 
-function showHouse() {
-    //
-    document.getElementById("data").innerHTML = house;
-}
+        var x = document.createElement("tr");
+        var y = document.createElement("th");
+        var z = document.createElement("div");
+        x.appendChild(y);
+        
+        
+        document.getElementsByClassName('table-striped').appendChild(x).appendChild(y).innerHTML = i
+        print(matchDay[i]);
+    }
 
-function showGender() {
-    //
-    document.getElementById("data").innerHTML = gender;
-}
 
-function showBook() {
-    //
-    document.getElementById("data").innerHTML = book;
-}
 
-function clearData() {
-    document.getElementById("data", "infoBox").innerHTML = clear;
-};
 
-getDataFromAPI(function(data) {
-    useApiData(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
